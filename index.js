e027fa7bed9611b855dadb7da31a63409e8257ab
Cryobyte56 <danielasanon54@gmail.com>
2025-04-29 06:52:33 +0800

    Finished Header Parser.

--- index.js
+++ index.js
@@ -21,7 +21,15 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software,
+  });
 });
 
 // listen for requests :)
