31e5393de3b00e0027f9317a51ed22c333c0500d
Cryobyte56 <danielasanon54@gmail.com>
2025-04-29 06:55:51 +0800

    Changed Endpoint from hello to whoami.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/whoami', function (req, res) {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
   const language = req.headers["accept-language"];
   const software = req.headers["user-agent"];
